75d59334c466e22847df5e6599a37d0f638668b9
Berno <bernostrubing@yahoo.com>
2025-11-17 18:20:27 +0200

    fix url

--- client/src/api.js
+++ client/src/api.js
@@ -1,7 +1,11 @@
 import axios from "axios";
 
+const production = window.location.hostname.includes("railway");
+
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || "http://localhost:5000"
+    baseURL: production
+        ? "https://fleetpro-backend-production.up.railway.app"
+        : "http://localhost:5000",
 });
 
 export default api;
