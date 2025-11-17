675c61ab2d8f6b7951c1a83e7226db1a3cb86cfd
Berno <bernostrubing@yahoo.com>
2025-11-17 19:52:17 +0200

    Fix backend URL correctly

--- client/src/api.js
+++ client/src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://fleetpro-backend-production.up.railway.app"
+    baseURL: "https://fleetpro-backend-production.up.railway.app"
 });
 
 export default api;
