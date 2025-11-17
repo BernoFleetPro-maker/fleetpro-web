496fa395a797d2464d273c65512a7170873a7dcc
Berno <bernostrubing@yahoo.com>
2025-11-17 19:34:19 +0200

    Fix API URL for production

--- client/src/api.js
+++ client/src/api.js
@@ -1,11 +1,7 @@
 import axios from "axios";
 
-const production = window.location.hostname.includes("railway");
-
 const api = axios.create({
-    baseURL: production
-        ? "https://fleetpro-backend-production.up.railway.app"
-        : "http://localhost:5000",
+   baseURL: "https://fleetpro-backend-production.up.railway.app"
 });
 
 export default api;
